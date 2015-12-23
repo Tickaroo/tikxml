231fa44de31ec167b9bc9ad75aef905ce954070b
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-12-23 10:40:35 +0100

    initial xml processor structure

---
+++ processor/src/main/java/com/tickaroo/tikxml/processor/XmlProcessor.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2015 Hannes Dorfmann
+ * Copyright (C) 2015 Tickaroo, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package com.tickaroo.tikxml.processor;
+
+import com.google.auto.service.AutoService;
+import com.tickaroo.tikxml.annotation.Xml;
+import java.util.HashSet;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+/**
+ * @author Hannes Dorfmann
+ */
+
+@AutoService(Processor.class)
+public class XmlProcessor extends AbstractProcessor {
+
+  private Messager messager;
+
+  @Override
+  public synchronized void init(ProcessingEnvironment processingEnv) {
+    super.init(processingEnv);
+    messager = processingEnv.getMessager();
+  }
+
+  @Override
+  public Set<String> getSupportedAnnotationTypes() {
+    Set<String> types = new HashSet<>();
+    types.add(Xml.class.getCanonicalName());
+    return types;
+  }
+
+  @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.latestSupported();
+  }
+
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+    Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(Xml.class);
+
+    for (Element elemenet : elementsAnnotatedWith) {
+      messager.printMessage(Diagnostic.Kind.ERROR, "Found element " + elemenet.toString());
+    }
+    return false;
+  }
+}
