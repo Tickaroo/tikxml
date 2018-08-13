e2b9fc047ff1a4347ff943dbbed8cdd25d2a5e98
Mariusz Saramak <mariusz.saramak@tomtom.com>
2018-08-13 09:03:31 +0200

    Regrestion tests for default namespace

--- annotationprocessingtesting/src/test/java/com/tickaroo/tikxml/annotationprocessing/namespace/NamespaceTest.java
+++ annotationprocessingtesting/src/test/java/com/tickaroo/tikxml/annotationprocessing/namespace/NamespaceTest.java
@@ -4,6 +4,8 @@ import com.tickaroo.tikxml.TestUtils;
 import com.tickaroo.tikxml.TikXml;
 import java.io.IOException;
 import org.junit.*;
+import java.text.ParseException;
+import okio.Buffer;
 
 /**
  * @author Hannes Dorfmann
@@ -19,5 +21,34 @@ public class NamespaceTest {
 
     Assert.assertEquals("Foo", item.name);
     Assert.assertEquals(123, item.child.id);
+  }
+
+  @Test
+  public void namespaceDefaultDefinitions() throws IOException {
+    TikXml xml = new TikXml.Builder().exceptionOnUnreadXml(false).build();
+
+    RootDefaultNamespace item =
+            xml.read(TestUtils.sourceForFile("regression/namespaces_default_fix.xml"), RootDefaultNamespace.class);
+
+    Assert.assertEquals("Foo", item.name);
+  }
+
+  @Test
+  public void generateXmlWithDefaultNamespace() throws IOException {
+    TikXml xml = new TikXml.Builder().exceptionOnUnreadXml(false).build();
+
+    RootDefaultNamespace item = new RootDefaultNamespace();
+    item.name = "Foo";
+    Buffer buffer = new Buffer();
+    xml.write(buffer, item);
+
+    String xmlStr =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?><rootDefaultNamespace xmlns=\"http://www.w3.org/1998/Math/MathML\"><name>Foo</name></rootDefaultNamespace>";
+    Assert.assertEquals(xmlStr, TestUtils.bufferToString(buffer));
+
+    RootDefaultNamespace item2 =
+            xml.read(TestUtils.sourceFrom(xmlStr), RootDefaultNamespace.class);
+    Assert.assertEquals(item.name, item2.name);
+
   }
 }
